93e8b37c34e84c6b6f9a2aa4f72cb18cf7047e24
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-16 00:01:03 +0100

    :fire: + moved to TypeScript

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+  </Layout>
+)
+
+export default IndexPage
