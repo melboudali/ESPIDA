2ca340b14856401570fcd5d50f8344727289b303
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-21 21:21:18 +0100

    :wrench: + gatsby-plugin-styled-components and gatsby-source-shopify

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 export default {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -7,7 +9,15 @@ export default {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: { placeholder: "blurred" },
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,17 +25,24 @@ export default {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
+        theme_color: `#fff`,
         background_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`,
+        cache_busting_mode: "none",
+      },
+    },
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        password: process.env.SHOPIFY_SHOP_PASSWORD,
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
       },
     },
   ],
