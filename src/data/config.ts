cddff3e54dd3d0584566a751ec5eedde05350027
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-22 22:57:31 +0100

    :sparkles: + config

---
+++ src/data/config.ts
@@ -0,0 +1,15 @@
+const config = {
+  title: "Espida Clothing",
+  description:
+    "Our mission is to outfit the world's most ambitious people. We design apparel with timeless style, versatility, and comfort in mind.",
+  siteUrl: "https://espida.elboudali.com",
+  featuredImage: "/images/thumbnail.png",
+  backgroundColor: `#191820`,
+  themeColor: `#16141F`,
+  icon: `src/assets/images/icon.jpg`,
+  fbid: "3820447614661427",
+  author: "",
+  googleAnalyticsTrackingID: "G-R5X3QX1HXG",
+};
+
+export default config;
