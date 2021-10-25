37660f50ee3905bd6816ae6596cf7857f0236a11
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-26 00:45:12 +0100

    :lipstick: -+ index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
-import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/common/seo";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
   </Layout>
 );
 
