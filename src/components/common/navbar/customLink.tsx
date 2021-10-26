13ecac6bb9c326f6372f0946e08267a7394d3c2c
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-26 23:02:15 +0100

    :lipstick: + :before and :hover selectors

--- src/components/common/navbar/customLink.tsx
+++ src/components/common/navbar/customLink.tsx
@@ -4,11 +4,29 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const CustomLinkWrapper = styled(Link)`
+  position: relative;
   color: var(--black);
   font-weight: 500;
   font-size: 0.9rem;
   line-height: 10px;
   text-transform: uppercase;
+  &:before {
+    content: "";
+    position: absolute;
+    width: 0;
+    bottom: -10px;
+    height: 2px;
+    background-color: var(--black);
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    transition: width 300ms ease-in-out;
+  }
+  &:hover {
+    &:before {
+      width: 100%;
+    }
+  }
 `;
 
 interface CustomLinkProps {
