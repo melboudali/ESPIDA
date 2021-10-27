657d31b15598df5e676206c7e5f55a32146aad69
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-27 22:16:37 +0100

    :lipstick: - layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Layout from "../components/layout";
 import Seo from "../components/common/seo";
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <Seo title="Home" />
-  </Layout>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum at placeat repudiandae illo! At ipsum optio in voluptates laboriosam
+      itaque nisi vel quis libero accusantium dolores officiis ipsam, minima ut atque architecto ad eligendi suscipit ab? Non commodi totam
+      eligendi fugiat provident, facilis ullam. Tempore cumque libero dolore qui vitae.
+    </p>
+  </>
 );
 
 export default IndexPage;
