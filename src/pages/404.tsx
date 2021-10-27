e7b428f12a069791f8e19203ad7e970ee932bd1f
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-27 22:15:50 +0100

    :lipstick: - layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,12 @@
-import * as React from "react";
-
-import Layout from "../components/layout";
+import React from "react";
 import Seo from "../components/common/seo";
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </>
 );
 
 export default NotFoundPage;
