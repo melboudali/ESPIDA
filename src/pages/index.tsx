61bfcf966444ec282019ca92024786c9a78bdf73
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-28 23:33:53 +0100

    :lipstick: -+ index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,13 @@ import styled from "styled-components";
 import { StaticImage } from "gatsby-plugin-image";
 
 const Slider = styled.div`
-  height: 611px;
+  height: 500px;
   width: 100%;
+  .myImage {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
 
 const IndexPage = () => (
@@ -15,6 +20,7 @@ const IndexPage = () => (
       <StaticImage
         src="https://images.unsplash.com/photo-1509631179647-0177331693ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1376&q=80"
         alt=""
+        className="myImage"
       />
     </Slider>
   </>
