58f0e41b6b8fdcc018e4a4d1a831e3bf71abdbed
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-31 20:18:05 +0000

    :art: - component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,13 @@ const Slider = styled.div`
 const IndexPage = () => (
   <>
     <Seo title="Home" />
-    <Slider>
+    {/* <Slider>
       <StaticImage
         src="https://images.unsplash.com/photo-1509631179647-0177331693ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1376&q=80"
         alt=""
         className="myImage"
       />
-    </Slider>
+    </Slider> */}
   </>
 );
 
