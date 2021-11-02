86c0f6159b23c27bc2a492e5b00c0df510b68ce7
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-02 18:58:12 +0000

    :sparkles: + categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,40 @@ import Seo from "../components/common/seo";
 import styled from "styled-components";
 import { StaticImage } from "gatsby-plugin-image";
 
-const Slider = styled.div`
-  height: 500px;
+const Categories = styled.div`
   width: 100%;
-  .myImage {
+  display: flex;
+  margin-top: 40px;
+  /* .myImage {
     width: 100%;
     height: 100%;
     object-fit: cover;
+  } */
+`;
+
+const Jackets = styled.div`
+  overflow: hidden;
+  .cat_img {
+    transform: scale(1.1);
+    transform-origin: 80% 100%;
   }
 `;
+
+const Others = styled.div``;
 
 const IndexPage = () => (
   <>
     <Seo title="Home" />
-    {/* FIXME: fix this later */}
-    {/* <Slider>
-      <StaticImage
-        src="https://images.unsplash.com/photo-1509631179647-0177331693ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1376&q=80"
-        alt=""
-        className="myImage"
-      />
-    </Slider> */}
+    <Categories>
+      <Jackets>
+        <StaticImage
+          src="https://images.unsplash.com/photo-1627637454030-5ddd536e06e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+          alt="Jackets"
+          className="cat_img"
+        />
+      </Jackets>
+      <Others></Others>
+    </Categories>
   </>
 );
 
