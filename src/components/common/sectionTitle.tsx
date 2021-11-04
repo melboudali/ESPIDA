63d1862ac15673c274bb88692814d99a6d755339
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-04 20:54:16 +0000

    :sparkles: + sectionTitle component

---
+++ src/components/common/sectionTitle.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 80px 0 60px;
+  color: var(--balck);
+  font-size: 1.8rem;
+  font-weight: 700;
+  line-height: 35px;
+  letter-spacing: 0.365em;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+interface SectionTitleProps {
+  title: string;
+}
+
+const SectionTitle = ({ title }: SectionTitleProps) => {
+  return <Title>{title}</Title>;
+};
+
+export default SectionTitle;
