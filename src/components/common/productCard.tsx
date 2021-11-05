b99938986bae40c774267a97e642e8cf83e03924
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-05 20:16:46 +0000

    :sparkles: + GatsbyImage

--- src/components/common/productCard.tsx
+++ src/components/common/productCard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { shopifyProduct } from "../../types";
-
+import { GatsbyImage } from "gatsby-plugin-image";
 const Card = styled.div`
-  width: 305px;
-  height: 300px;
-  background-color: red;
-  border-radius: 10px;
+  flex: 305px 4 1;
+  .gatsby_image {
+    width: 100%;
+    height: 384px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
 `;
 
 interface ProductCardProps {
@@ -14,7 +17,12 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
-  return <Card>{product.title}</Card>;
+  return (
+    <Card>
+      <GatsbyImage image={product.images![0]!.gatsbyImageData} alt={product.images![0]!.altText!} className="gatsby_image" />
+      {product.title}
+    </Card>
+  );
 };
 
 export default ProductCard;
