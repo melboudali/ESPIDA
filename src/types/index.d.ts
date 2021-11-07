165c311a4d9bdb6598152d052021057dd6f5e4d4
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-07 20:25:50 +0000

    :label: + priceRangeV2 -> shopifyProduct type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,4 +5,11 @@ export type shopifyProduct = {
   publishedAt?: any | null | undefined;
   collections?: Array<{ title?: string | null | undefined } | null | undefined> | null | undefined;
   images?: Array<{ altText?: string | null | undefined; gatsbyImageData: any } | null | undefined> | null | undefined;
+  priceRangeV2?:
+    | {
+        maxVariantPrice?: { amount?: string | null | undefined } | null | undefined;
+        minVariantPrice?: { amount?: string | null | undefined } | null | undefined;
+      }
+    | null
+    | undefined;
 };
