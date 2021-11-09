f70f7ea58ad59cc7f3d5c72d9416784218d468f3
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-09 16:19:41 +0000

    :sparkles: + TitleWithBg

--- src/components/common/sectionTitle.tsx
+++ src/components/common/sectionTitle.tsx
@@ -1,5 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const TitleWithBg = styled.div`
+  margin: 0 0 60px;
+  width: 100%;
+  height: 589px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 10px;
+  z-index: 1;
+  .img {
+    transform: scale(1.1);
+    transform-origin: 85% 380%;
+  }
+`;
 
 const Title = styled.h1`
   margin: 0;
@@ -14,9 +28,12 @@ const Title = styled.h1`
 
 interface SectionTitleProps {
   title: string;
+  children?: React.ReactNode;
+  link?: string;
 }
 
-const SectionTitle = ({ title }: SectionTitleProps) => {
+const SectionTitle = ({ title, children, link }: SectionTitleProps) => {
+  if (children) return <TitleWithBg>{children}</TitleWithBg>;
   return <Title>{title}</Title>;
 };
 
