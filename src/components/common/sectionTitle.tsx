5d3d01f944b359d77bc46fe250e5881ac1ab79a0
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-09 17:49:22 +0000

    :lipstick: -+ TitleWithBg

--- src/components/common/sectionTitle.tsx
+++ src/components/common/sectionTitle.tsx
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react";
 import styled, { css } from "styled-components";
 
@@ -10,8 +11,9 @@ const TitleWithBg = styled.div`
   border-radius: 10px;
   z-index: 1;
   .img {
-    transform: scale(1.1);
-    transform-origin: 85% 380%;
+    transform: scale(1.01);
+    transform-origin: 10% 3100%;
+    z-index: 2;
   }
 `;
 
@@ -26,6 +28,39 @@ const Title = styled.h1`
   text-align: center;
 `;
 
+const Content = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 3;
+  h1 {
+    margin: 0;
+    font-weight: 700;
+    font-size: 6rem;
+    line-height: 111px;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: var(--white);
+  }
+  a {
+    font-weight: 700;
+    font-size: 1.125rem;
+    line-height: 21px;
+    letter-spacing: 0.365em;
+    text-transform: uppercase;
+    color: var(--white);
+    padding: 21px 73px;
+    border: 4px solid var(--white);
+  }
+`;
+
 interface SectionTitleProps {
   title: string;
   children?: React.ReactNode;
@@ -33,7 +68,16 @@ interface SectionTitleProps {
 }
 
 const SectionTitle = ({ title, children, link }: SectionTitleProps) => {
-  if (children) return <TitleWithBg>{children}</TitleWithBg>;
+  if (children && link)
+    return (
+      <TitleWithBg>
+        {children}
+        <Content>
+          <h1>{title}</h1>
+          <Link to={link}>shop now</Link>
+        </Content>
+      </TitleWithBg>
+    );
   return <Title>{title}</Title>;
 };
 
