e3df7c5e97317fb18dddeb1408f92cd8b933540b
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-09 17:50:51 +0000

    :lipstick: + SectionContainer + newReleases section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import ProductCard from "../components/common/productCard";
 import { graphql } from "gatsby";
 import { AllShopifyProductQuery } from "../../gatsby-graphql";
 import { shopifyProduct } from "../types";
+import { StaticImage } from "gatsby-plugin-image";
 
-const CardsSection = styled.section`
+const SectionContainer = styled.section`
   margin: 80px 0;
 `;
 const Cards = styled.div`
@@ -25,14 +26,28 @@ const IndexPage = ({ data: { bestSellers, newReleases } }: IndexPageProps) => (
   <>
     <Seo title="Home" />
     <Categories />
-    <CardsSection>
+    <SectionContainer>
       <SectionTitle title="Best Sellers" />
       <Cards>
         {bestSellers.nodes.map((node: shopifyProduct) => (
           <ProductCard product={node} />
         ))}
       </Cards>
-    </CardsSection>
+    </SectionContainer>
+    <SectionContainer>
+      <SectionTitle title="new releases" link="/collections/new-releases">
+        <StaticImage
+          src="https://images.unsplash.com/photo-1542219550-2da790bf52e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
+          alt="new releases"
+          className="img"
+        />
+      </SectionTitle>
+      <Cards>
+        {newReleases.nodes.map((node: shopifyProduct) => (
+          <ProductCard product={node} />
+        ))}
+      </Cards>
+    </SectionContainer>
   </>
 );
 
