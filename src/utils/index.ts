ad0033f86cf24383a8c816b4ce55c047ba741202
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-11 22:42:52 +0000

    :fire: -.map +for of

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,12 +2,12 @@ import { variantsType } from "../types";
 
 export const getColorsAndImages = (variants: variantsType) => {
   const selectedVariants: { color: string; image: any }[] = [];
-  variants!.map((variant) => {
+  for (const variant of variants) {
     const colorName: string = variant!.title!.split(" /")[0];
     if (selectedVariants.findIndex((selectedVariant) => selectedVariant.color === colorName) === -1) {
       selectedVariants.push({ color: colorName, image: variant!.image!.gatsbyImageData });
     }
-  });
+  }
 
   return selectedVariants;
 };
