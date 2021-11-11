c34fc2bd6358abdc4ff5a86a54d95dc108a742db
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-11 22:36:19 +0000

    :label: + variantsType

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,33 @@
+type variantsType = Array<
+  | {
+      price?: string | null | undefined;
+      compareAtPrice?: string | null | undefined;
+      title?: string | null | undefined;
+      image?: { gatsbyImageData: any } | null | undefined;
+    }
+  | null
+  | undefined
+>;
+
 export type shopifyProduct = {
+  id: string;
   title?: string | null | undefined;
   description?: string | null | undefined;
   tags?: Array<string | null | undefined> | null | undefined;
   publishedAt?: any | null | undefined;
   collections?: Array<{ title?: string | null | undefined } | null | undefined> | null | undefined;
   images?: Array<{ altText?: string | null | undefined; gatsbyImageData: any } | null | undefined> | null | undefined;
-  variants?: Array<{ price?: string | null | undefined; compareAtPrice?: string | null | undefined } | null | undefined> | null | undefined;
+  variants?:
+    | Array<
+        | {
+            price?: string | null | undefined;
+            compareAtPrice?: string | null | undefined;
+            title?: string | null | undefined;
+            image?: { gatsbyImageData: any } | null | undefined;
+          }
+        | null
+        | undefined
+      >
+    | null
+    | undefined;
 };
