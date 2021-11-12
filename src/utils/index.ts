039224c32441a307f9c1fbbeb7a013e51e97c739
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-12 18:53:41 +0000

    :sparkles: + colots and getColor function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,6 +12,26 @@ export const getColorsAndImages = (variants: variantsType) => {
   return selectedVariants;
 };
 
-const getColor = (color: string) => {
-  const colors = [{ black: "#000", white: "#fff" }];
+const colorsHex: { color: string; hex: string }[] = [
+  { color: "black", hex: "#000" },
+  { color: "grey", hex: "#808080" },
+  { color: "white", hex: "#fff" },
+  { color: "red", hex: "#ff0000" },
+  { color: "brown", hex: "#a52a2a" },
+  { color: "burgundy", hex: "#800020" },
+  { color: "blue", hex: "#0000ff" },
+  { color: "dark blue", hex: "#00008b" },
+  { color: "navy", hex: "#000080" },
+  { color: "purple", hex: "#800080" },
+  { color: "yellow  ", hex: "#ffff00" },
+  { color: "khaki", hex: "#f0e68c" },
+  { color: "light khaki", hex: "#f6f0ba" },
+  { color: "beige", hex: "#f5f5dc" },
+];
+
+export const getColor = (color: string) => {
+  for (const colorHex of colorsHex) {
+    if (color === colorHex.color) return colorHex.hex;
+  }
+  return "#ff0000";
 };
