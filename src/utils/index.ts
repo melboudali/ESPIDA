90cf586d9029e6d1e5b9e6d0b919dde729fb4d2c
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-14 18:57:48 +0000

    :lipstick: -+ colors + destructuring

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,22 +16,22 @@ const colorsHex: { color: string; hex: string }[] = [
   { color: "black", hex: "#000" },
   { color: "grey", hex: "#808080" },
   { color: "white", hex: "#fff" },
-  { color: "red", hex: "#ff0000" },
-  { color: "brown", hex: "#a52a2a" },
+  { color: "red", hex: "#c93030" },
+  { color: "brown", hex: "#541906" },
   { color: "burgundy", hex: "#800020" },
-  { color: "blue", hex: "#0000ff" },
-  { color: "dark blue", hex: "#00008b" },
-  { color: "navy", hex: "#000080" },
-  { color: "purple", hex: "#800080" },
-  { color: "yellow", hex: "#ffff00" },
+  { color: "blue", hex: "#4848cf" },
+  { color: "dark blue", hex: "#222270" },
+  { color: "navy", hex: "#222e4d" },
+  { color: "purple", hex: "#872d87" },
+  { color: "yellow", hex: "#fcfc6f" },
   { color: "khaki", hex: "#f0e68c" },
   { color: "light khaki", hex: "#f6f0ba" },
   { color: "beige", hex: "#f5f5dc" },
 ];
 
-export const getColor = (color: string) => {
-  for (const colorHex of colorsHex) {
-    if (color.toLowerCase() === colorHex.color) return colorHex.hex;
+export const getColor = (colorName: string) => {
+  for (const { color, hex } of colorsHex) {
+    if (colorName.toLowerCase() === color) return hex;
   }
   return "#ff0000";
 };
