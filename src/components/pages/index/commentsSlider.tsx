6a680f8ac18d9d1c31f83a2641ca0d0ab750bb33
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-16 23:47:36 +0000

    :lipstick: + CommentsSliderWrapper

--- src/components/pages/index/commentsSlider.tsx
+++ src/components/pages/index/commentsSlider.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const CommentsSliderWrapper = styled.div`
+  background-color: #fafafa;
+  margin-top: 40px;
+  padding: 63px 0;
+`;
 
 interface CommentsSliderProps {}
 
 const CommentsSlider: React.FC<CommentsSliderProps> = (props) => {
-  return <div>This is commentsSlider Component/Page</div>;
+  return <CommentsSliderWrapper>This is commentsSlider Component/Page</CommentsSliderWrapper>;
 };
 
 CommentsSlider.propTypes = {};
