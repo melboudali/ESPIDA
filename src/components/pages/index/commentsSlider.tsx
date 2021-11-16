5c5cfb80ee5005b798960c19bd4811bb2940bb1b
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-16 23:13:21 +0000

    :zap: + commentSlider

---
+++ src/components/pages/index/commentsSlider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+interface commentsSliderProps {}
+
+const commentsSlider: React.FC<commentsSliderProps> = (props) => {
+  return <div>This is commentsSlider Component/Page</div>;
+};
+
+commentsSlider.propTypes = {};
+
+export default commentsSlider;
