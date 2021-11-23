509b52a33658668575ef81cef922670f849b93db
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-23 22:19:17 +0000

    :wrench: add gatsby-plugin-mailchimp

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -57,5 +57,12 @@ export default {
         documentPaths: ["./src/**/*.{ts,tsx}", "./node_modules/gatsby-*/**/*.js"],
       },
     },
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint: process.env.MAILCHIMP_ENDPOINT, // string; add your MC list endpoint here; see instructions below
+        timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
+    },
   ],
 };
