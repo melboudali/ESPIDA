ec28e2beea5283c4ee558347301febf7baf5cf2a
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-24 15:51:31 +0000

    :sparkles: add validateEmail func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -36,3 +36,9 @@ export const getColor = (colorName: string) => {
   }
   return "#c93030";
 };
+
+export const validateEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
