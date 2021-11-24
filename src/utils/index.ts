3e26cbda6c57dca1b428a2ef36576ca98ac0784c
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-24 20:46:11 +0000

    :bug: -+ validateEmail

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -40,5 +40,5 @@ export const getColor = (colorName: string) => {
 export const validateEmail = (email: string) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  return re.test(String(email).toLowerCase()) && !!email;
 };
