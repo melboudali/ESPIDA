39511f97ec0a57e153d53f1855f8ac1f550304e8
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-11-25 20:38:56 +0000

    :lipstick: -+ footer

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -7,7 +7,20 @@ const SectionContainer = styled.section`
   margin: 80px 0;
 `;
 
-const FooterWrapper = styled.footer``;
+const FooterWrapper = styled.footer`
+  background-color: var(--black);
+  height: 600px;
+  margin-bottom: -110px;
+  &:after {
+    content: "";
+    background-color: var(--white);
+    background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQzOSIgaGVpZ2h0PSIxMTgiIHZpZXdCb3g9IjAgMCAxNDM5IDExOCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgNC4yOTIzMUM0OTIuNSAtMzEuODQzOSAxMTEzLjUgMTc3LjA1NCAxNDQwIDQuMjkyMzFWMTE4SDBWNC4yOTIzMVoiIGZpbGw9ImJsYWNrIi8+Cjwvc3ZnPgo=);
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 118px;
+    display: block;
+  }
+`;
 
 const Footer = () => {
   return (
