f35f9465b00b4cbdfd3915d6521ffe933c30a9e1
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-12-01 22:21:22 +0000

    :label: edited handle and productType

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -29,6 +29,8 @@ export type shopifyProduct = {
   title?: string | null | undefined;
   description?: string | null | undefined;
   tags?: Array<string | null | undefined> | null | undefined;
+  handle?: string | null | undefined;
+  productType?: string | null | undefined;
   publishedAt?: any | null | undefined;
   collections?: Array<{ title?: string | null | undefined } | null | undefined> | null | undefined;
   images?: Array<{ altText?: string | null | undefined; gatsbyImageData: any } | null | undefined> | null | undefined;
