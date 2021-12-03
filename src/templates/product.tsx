5f55ad6886e63424ccd665564dc0bfcdd3136eda
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-12-03 21:13:27 +0000

    :art: add grqphql query

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -1,13 +1,34 @@
 import { graphql } from "gatsby";
 import React from "react";
+import styled from "styled-components";
 import { ShopifyProductQuery } from "../../gatsby-graphql";
+
+const ProductWrapper = styled.section`
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Images = styled.div`
+  flex: 2;
+`;
+
+const Details = styled.div`
+  flex: 1;
+`;
 
 interface productProps {
   data: ShopifyProductQuery;
 }
 
 const product = ({ data: { productData } }: productProps) => {
-  return <div>hello world {productData?.title}</div>;
+  return (
+    <ProductWrapper>
+      <Images>hello world {productData?.title}</Images>
+      <Details>hello world {productData?.title}</Details>
+    </ProductWrapper>
+  );
 };
 
 export default product;
