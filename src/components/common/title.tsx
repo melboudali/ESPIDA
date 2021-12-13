50de7bb04510eeef7d56e26d1d5e46065c070d7e
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-12-13 21:42:06 +0000

    :iphone: add responsive design

--- src/components/common/title.tsx
+++ src/components/common/title.tsx
@@ -4,17 +4,24 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const TitleWithBg = styled.div`
-  margin: 0 0 60px;
-  width: 100%;
-  height: 589px;
+  --width: calc(100% - 20px);
+  --height: 300px;
+  margin: 0 auto 60px;
+  width: var(--width);
+  height: var(--height);
   position: relative;
   overflow: hidden;
   border-radius: 10px;
   z-index: 1;
+
   .img {
-    transform: scale(1.01);
-    transform-origin: 10% 3100%;
-    z-index: 2;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+  @media (min-width: 750px) {
+    --width: 100%;
+    --height: 589px;
   }
 `;
 
@@ -30,6 +37,9 @@ const Title = styled.h1<{ fontSize: string }>`
 `;
 
 const Content = styled.div`
+  --fontSize: 2rem;
+  --padding: 15px 50px;
+
   position: absolute;
   display: flex;
   align-items: center;
@@ -44,7 +54,7 @@ const Content = styled.div`
   h1 {
     margin: 0;
     font-weight: 700;
-    font-size: 6rem;
+    font-size: var(--fontSize);
     line-height: 111px;
     letter-spacing: 0.1em;
     text-transform: uppercase;
@@ -57,8 +67,12 @@ const Content = styled.div`
     letter-spacing: 0.365em;
     text-transform: uppercase;
     color: var(--white);
-    padding: 21px 73px;
+    padding: var(--padding);
     border: 4px solid var(--white);
+  }
+  @media (min-width: 750px) {
+    --fontSize: 6rem;
+    --padding: 21px 73px;
   }
 `;
 
