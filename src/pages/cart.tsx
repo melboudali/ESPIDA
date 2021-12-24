1f793c27990072a0923e4345db0719cd5d8d8377
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-12-24 20:48:29 +0000

    :sparkles: add items

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,3 +1,4 @@
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { StoreContext } from "../context";
 
@@ -11,20 +12,30 @@ const cart = (props: Props) => {
   const quantity = items.reduce((total: any, item: any) => {
     return total + item.quantity;
   }, 0);
-
+  console.log(items[0]);
   return (
     <div>
       you have {quantity} item
       <a href={checkout.webUrl} target="_blank">
         click me
       </a>
-      <div>
-        {checkout.lineItems.map((qtty: any) => (
-          <p>
-            {qtty.title} {qtty.variant.title}
-          </p>
-        ))}
-      </div>
+      {items && (
+        <div>
+          {checkout.lineItems.map((qtty: any) => (
+            <div key={qtty.id}>
+              <p>title: {qtty.title}</p>
+              <p>quantity: {qtty.quantity}</p>
+              <p>variant id: {qtty.variant.id}</p>
+              {/* {qtty.variant.image && (
+                <GatsbyImage image={qtty.variant.image} alt="product_image" className="gatsby_image" style={{ width: "100px" }} />
+              )} */}
+              <img src={qtty.variant.image.src} alt="jackets" style={{ width: "100px" }} />
+              <p>variant title: {qtty.variant.title}</p>
+              <p>variant price: {qtty.variant.price}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
