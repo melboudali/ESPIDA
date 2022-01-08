c74b7f5fbcf647c3a9579d537ed81665702bb877
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-08 21:19:00 +0000

    :wrench: added siteUrl and fbid

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,10 +2,12 @@ const config = {
   title: "Espida Clothing",
   description:
     "Our mission is to outfit the world's most ambitious people. We design apparel with timeless style, versatility, and comfort in mind.",
+  siteUrl: "https://espida.elboudali.com",
   featuredImage: "/images/thumbnail.png",
   backgroundColor: `#191820`,
   themeColor: `#16141F`,
   icon: `src/assets/images/icon.jpg`,
+  fbid: "3820447614661427",
   author: "moelboudali",
   googleAnalyticsTrackingID: "G-YE1MC1H3CR",
 };
