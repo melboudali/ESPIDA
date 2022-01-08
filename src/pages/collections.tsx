5465410065b2e1c3cf9216d0b59642fc731e8857
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-08 13:08:35 +0000

    :label: added allCollectionsProducts

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -1,9 +1,40 @@
+import { graphql } from "gatsby";
 import React from "react";
+import { AllCollectionsProductsQuery } from "../../gatsby-graphql";
 
-interface Props {}
+interface CollectionsProps {
+  data: AllCollectionsProductsQuery;
+}
 
-const Collections = (props: Props) => {
+const Collections = ({ data: { allShopifyProduct } }: CollectionsProps) => {
   return <div>Collections page</div>;
 };
 
 export default Collections;
+
+export const query = graphql`
+  query allCollectionsProducts {
+    allShopifyProduct(sort: { order: DESC, fields: createdAt }) {
+      nodes {
+        id
+        title
+        handle
+        productType
+        images {
+          altText
+          gatsbyImageData
+        }
+        variants {
+          id
+          title
+          storefrontId
+          price
+          compareAtPrice
+          image {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`;
