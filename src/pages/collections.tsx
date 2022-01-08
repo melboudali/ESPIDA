6b4034a1cbdce670e5f60b389633bb4e5c5c2e6b
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-08 14:22:34 +0000

    :sparkles: added PageTitle, Cards, and wrapper

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -1,13 +1,42 @@
 import { graphql } from "gatsby";
 import React from "react";
+import styled from "styled-components";
 import { AllCollectionsProductsQuery } from "../../gatsby-graphql";
+import ProductCard from "../components/common/card";
+import { PageTitle } from "../components/common/pageTitle";
+import { shopifyProduct } from "../types";
+
+const Wrapper = styled.section`
+  margin-top: 40px;
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 60px;
+`;
 
 interface CollectionsProps {
   data: AllCollectionsProductsQuery;
 }
 
-const Collections = ({ data: { allShopifyProduct } }: CollectionsProps) => {
-  return <div>Collections page</div>;
+const Collections = ({
+  data: {
+    allShopifyProduct: { nodes },
+  },
+}: CollectionsProps) => {
+  return (
+    <Wrapper>
+      <PageTitle quantity={nodes.length}>collections</PageTitle>
+      <Cards>
+        {nodes.map((node: shopifyProduct) => (
+          <ProductCard key={node.id} product={node} />
+        ))}
+      </Cards>
+    </Wrapper>
+  );
 };
 
 export default Collections;
@@ -35,6 +64,7 @@ export const query = graphql`
           }
         }
       }
+      totalCount
     }
   }
 `;
