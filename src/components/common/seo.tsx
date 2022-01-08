3451b268408960c9d86b32005b48a29a2e6047f0
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-08 21:18:17 +0000

    :mag: updated Seo

--- src/components/common/seo.tsx
+++ src/components/common/seo.tsx
@@ -1,101 +1,69 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
+import { Helmet } from "react-helmet";
+import PropTypes from "prop-types";
 
 interface SeoProps {
-  title: string;
-  description?: string;
+  title?: string;
   image?: string;
-  lang?: string;
+  description?: string;
+  children?: React.ReactNode;
+  location?: string;
 }
 
-function Seo({ description, lang, title, image }: SeoProps) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-            image
-          }
+const Seo = ({ title, image, description, location }: SeoProps) => {
+  const { site }: any = useStaticQuery(graphql`
+    query mySiteMetaData {
+      site {
+        siteMetadata {
+          defaultTitle: title
+          defaultDescription: description
+          defaultImage: image
+          siteUrl
+          author
+          fbid
         }
       }
-    `
-  );
-  const defaultImage = image || site.siteMetadata.image;
-  const metaDescription = description || site.siteMetadata.description;
-  const defaultTitle = site.siteMetadata?.title;
+    }
+  `);
+
+  const seo = {
+    title: title ?? site?.siteMetadata?.defaultTitle,
+    description: description ?? site?.siteMetadata?.defaultDescription,
+    image: `${site?.siteMetadata?.siteUrl}${image ?? site?.siteMetadata?.defaultImage}`,
+    siteUrl: `${site?.siteMetadata?.siteUrl}${location ?? ""}`,
+  };
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : ""}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          name: `image`,
-          content: defaultImage,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: defaultImage,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:image`,
-          content: defaultImage,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]}
-    />
-  );
-}
+    <Helmet>
+      <html lang="en" dir="ltr" />
+      <link rel="canonical" href={seo.siteUrl!} />
+      <link rel="preload" as="font" />
+      <title>{seo.title!}</title>
+      <meta name="description" content={seo.description!} />
+      {seo.image && <meta name="image" content={seo.image} />}
 
-Seo.defaultProps = {
-  lang: `en`,
-  description: ``,
+      <meta property="og:url" content={seo.siteUrl!} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={seo.title!} />
+      <meta property="og:description" content={seo.description!} />
+      {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta property="fb:app_id" content={site?.siteMetadata?.fbid!} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={site?.siteMetadata?.author!} />
+      <meta name="twitter:title" content={seo.title!} />
+      <meta name="twitter:description" content={seo.description!} />
+      {seo.image && <meta name="twitter:image:src" content={seo.image} />}
+    </Helmet>
+  );
 };
 
 Seo.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  image: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   description: PropTypes.string,
-  lang: PropTypes.string,
-  image: PropTypes.string,
+  location: PropTypes.string,
 };
 
 export default Seo;
