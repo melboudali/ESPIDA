e2072e24f82a2d2693669430bf7cdec42e480bfb
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-08 15:33:59 +0000

    :fire: updated turnCollectionsIntoPages and turnProductsIntoPages functions

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,11 +1,11 @@
 import path from "path";
 import { CreatePagesArgs } from "gatsby";
-import { resType, postType } from "./src/types";
+import { resPostType, postType, resCollectionType, collectionType } from "./src/types";
 
 const turnProductsIntoPages = async (args: CreatePagesArgs) => {
   const { actions, graphql } = args;
   const postTemplate = path.resolve("./src/templates/product.tsx");
-  const res: resType = await graphql(`
+  const res: resPostType = await graphql(`
     query createShopifyProduct {
       products: allShopifyProduct {
         nodes {
@@ -17,15 +17,39 @@ const turnProductsIntoPages = async (args: CreatePagesArgs) => {
     }
   `);
 
-  res.data?.products.nodes.forEach((post: postType) => {
+  res.data?.products.nodes.forEach(({ id, productType, handle }: postType) => {
     actions.createPage({
-      path: `collections/${post.productType!}/${post.handle!}`,
+      path: `collections/${productType}/${handle}`,
       component: postTemplate,
-      context: { id: post.id },
+      context: { id },
+    });
+  });
+};
+
+const turnCollectionsIntoPages = async (args: CreatePagesArgs) => {
+  const { actions, graphql } = args;
+  const collectionTemplate = path.resolve("./src/templates/collection.tsx");
+  const res: resCollectionType = await graphql(`
+    query createShopifyCollection {
+      collections: allShopifyCollection {
+        nodes {
+          id
+          title
+          handle
+        }
+      }
+    }
+  `);
+
+  res.data?.collections.nodes.forEach(({ id, handle, title }: collectionType) => {
+    actions.createPage({
+      path: `collections/${handle}`,
+      component: collectionTemplate,
+      context: { id, title },
     });
   });
 };
 
 exports.createPages = async (params: CreatePagesArgs) => {
-  await Promise.all([turnProductsIntoPages(params)]);
+  await Promise.all([turnProductsIntoPages(params), turnCollectionsIntoPages(params)]);
 };
