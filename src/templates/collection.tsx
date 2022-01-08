1b480bc6a894d540b951e98715ec4f7d3d2d13f4
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-08 18:24:43 +0000

    :sparkles: added graphql query, PageTitle, Cards, and ProductCard

--- src/templates/collection.tsx
+++ src/templates/collection.tsx
@@ -1,16 +1,74 @@
+import { graphql } from "gatsby";
 import React from "react";
+import { CollectionQuery } from "../../gatsby-graphql";
+import styled from "styled-components";
+import PageTitle from "../components/common/pageTitle";
+import { shopifyProduct } from "../types";
+import ProductCard from "../components/common/card";
+
+const Cards = styled.div`
+  --justifyContent: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: var(--justifyContent);
+  gap: 20px;
+  margin-top: 60px;
+  @media (min-width: 750px) {
+    --justifyContent: start;
+  }
+`;
 
 interface Props {
   pageContext: { id: string; title: string };
+  data: CollectionQuery;
 }
 
-const Collection = ({ pageContext: { id, title } }: Props) => {
+const Collection = ({
+  pageContext: { title },
+  data: {
+    collectionProducts: { nodes },
+  },
+}: Props) => {
   return (
-    <div>
-      <p>id: {id}</p>
-      <p>{title}</p>
-    </div>
+    <>
+      <PageTitle quantity={nodes.length}>{title}</PageTitle>
+      <Cards>
+        {nodes.map((node: shopifyProduct) => (
+          <ProductCard key={node.id} product={node} />
+        ))}
+      </Cards>
+    </>
   );
 };
 
 export default Collection;
+
+export const query = graphql`
+  query collection($id: String) {
+    collectionProducts: allShopifyProduct(
+      filter: { collections: { elemMatch: { id: { eq: $id } } } }
+      sort: { order: DESC, fields: createdAt }
+    ) {
+      nodes {
+        id
+        title
+        handle
+        productType
+        images {
+          altText
+          gatsbyImageData
+        }
+        variants {
+          id
+          title
+          storefrontId
+          price
+          compareAtPrice
+          image {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`;
