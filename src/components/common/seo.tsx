2aba96598c0ac87f2b4992796a5455f6abe3d109
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-09 23:41:25 +0000

    :label: added PropTypes

--- src/components/common/seo.tsx
+++ src/components/common/seo.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { Helmet } from "react-helmet";
-import PropTypes from "prop-types";
 import { MySiteMetaDataQuery } from "../../../gatsby-graphql";
+import PropTypes from "prop-types";
 
 interface SeoProps {
   title?: string;
