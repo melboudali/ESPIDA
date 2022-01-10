98bd2112d6f6bb2c10ac7c1ea00253a65ffdba4e
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-10 23:46:30 +0000

    :label: added types

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Client from "shopify-buy";
 
 const client = Client.buildClient({
-  storefrontAccessToken: process.env.GATSBY_STOREFRONT_ACCESS_TOKEN!,
-  domain: process.env.GATSBY_SHOPIFY_STORE_URL!,
+  storefrontAccessToken: process.env.GATSBY_STOREFRONT_ACCESS_TOKEN as string,
+  domain: process.env.GATSBY_SHOPIFY_STORE_URL as string,
 });
 
 interface defualtValuesType {
