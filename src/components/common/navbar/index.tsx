486bbda1a5171598fa89b5055cbb12179635bde0
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-01-11 13:19:24 +0000

    :wheelchair: added aria-label

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -108,7 +108,7 @@ const Navbar = () => {
   return (
     <NavbarWrapper>
       <Nav>
-        <Menu>
+        <Menu aria-label="menu">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2 2H22" stroke="black" strokeWidth="3" strokeLinecap="round" />
             <path d="M2 9H16" stroke="black" strokeWidth="3" strokeLinecap="round" />
